5b715aede3ac2d763657087f14d6e7a55a695c1d
Benjamin Joseph <benjo@sayhello.com>
2017-01-13 14:46:57 -0800

    matio has been added

--- haltija/haltijarunner.cpp
+++ haltija/haltijarunner.cpp
@@ -1,6 +1,10 @@
 #include "example.h"
 
-int main() {
- 
+int main(int argc, char * argv[]) {
+	//TODO read input CSV file or whatever format the radar is
+
+	//Process data
+
+	//output the result
   return 0;
 }
