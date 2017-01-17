a808e52c4c9ba490cd3d828766ac37d1f5153a9a
Benjamin Joseph <benjo@sayhello.com>
2017-01-17 11:21:44 -0800

    finding unit-test data now in windows

--- haltija/unit-test/testmatio.cpp
+++ haltija/unit-test/testmatio.cpp
@@ -16,22 +16,37 @@ protected:
 
 class DISABLED_TestMatio : public TestMatio {};
 
+#define GET_TEST_FILE_PATH(x)\
+    UNIT_TEST_DATA + std::string(x)
 
 TEST_F(TestMatio, OpenAndClose) {
-    mat_t * matfile = Mat_Open("RecX4_BB_Frames_20161028_170759.mat", MAT_ACC_RDONLY);
+	std::string filepath = GET_TEST_FILE_PATH("RecX4_BB_Frames_20161028_170759.mat");
+	mat_t * matfile = Mat_Open(filepath.c_str(), MAT_ACC_RDONLY);
 	ASSERT_TRUE(matfile != NULL);
 	Mat_Close(matfile);
 }
 
-/*
+
 TEST_F(TestMatio, TestReadingStuff) {
-	//mat_t * matfile = Mat_Open("RecX4_BB_Frames_20161028_170759.mat", MAT_ACC_RDONLY);
+	std::string filepath = GET_TEST_FILE_PATH("RecX4_BB_Frames_20161028_170759.mat");
+	mat_t * matfile = Mat_Open(filepath.c_str(), MAT_ACC_RDONLY);
 
-	//ASSERT_TRUE(matfile != NULL);
+	ASSERT_TRUE(matfile != NULL);
 
-    //matvar_t * foo = Mat_VarReadInfo(matfile, "RecFrames");
+	
+    matvar_t * frames = Mat_VarReadInfo(matfile, "RecFrames");
+	char * const * field_names = Mat_VarGetStructFieldnames(frames);
+	int n = Mat_VarGetNumberOfFields(frames);
+
+	for (int i = 0; i < n; i++) {
+		std::cout << field_names[i] << std::endl;
+	}
+	
+
+
+
     
 
-	//Mat_Close(matfile);
+	Mat_Close(matfile);
 }
-*/
+
