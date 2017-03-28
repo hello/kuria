906133b934cfee3fdd9ce3304cb03c390b6df24f
Mari Zakrzewski <mari@sayhello.com>
2017-03-27 20:23:32 -0700

    The very first peak detection version. The IIR filter does not work, or I'm using it wrongly.

---
+++ haltija/algorithm/peakFinding.cpp
@@ -0,0 +1,9 @@
+//
+//  peakFinding.cpp
+//  Haltija
+//
+//  Created by Mari Zakrzewski on 3/21/17.
+//
+//
+
+#include "peakFinding.hpp"
---
+++ haltija/algorithm/peakFinding.hpp
@@ -0,0 +1,14 @@
+//
+//  peakFinding.hpp
+//  Haltija
+//
+//  Created by Mari Zakrzewski on 3/21/17.
+//
+//
+
+#ifndef peakFinding_hpp
+#define peakFinding_hpp
+
+#include <stdio.h>
+
+#endif /* peakFinding_hpp */
