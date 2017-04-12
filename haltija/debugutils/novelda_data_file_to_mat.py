fd74ae89312f4b90d1992e8fdc36b3e581712178
Benjamin Joseph <benjo@sayhello.com>
2017-04-12 16:08:41 -0700

    this script should be transforming a file of protobufs to a mat file

---
+++ haltija/debugutils/novelda_data_file_to_mat.py
@@ -0,0 +1,34 @@
+import novelda_pb2
+import sys
+import base64
+import scipy.io
+import numpy as np
+def main():
+
+    if len(sys.argv) < 3:
+        print 'you need an input and an output file'
+        return 
+
+    data = {'range_bins' : [], 'frame_id' : [], 'base_band' : []}
+
+    with open(sys.argv[1]) as f:
+        for line in f:
+            rf = novelda_pb2.RadarFrame()
+            rf.ParseFromString(base64.b64decode(line))
+
+            v = np.array([f for f in rf.range_bins])
+            v2 = v[0::2] + v[1::2]*1j
+            
+            data['range_bins'].append(v2)
+
+            if rf.HasField('base_band'):
+                data['base_band'].append(int(rf.base_band))
+
+            if rf.HasField('frame_id'):
+                data['frame_id'].append(rf.frame_id)
+            
+     
+    scipy.io.savemat(sys.argv[2],data)
+            
+if __name__ == '__main__':
+    main()
