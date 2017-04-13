c5c168b97d231862f683bcab2581712fc5ad7d19
Benjamin Joseph <benjo@sayhello.com>
2017-04-12 18:02:24 -0700

    adds padding to base64 encoded strings

--- haltija/debugutils/novelda_data_file_to_mat.py
+++ haltija/debugutils/novelda_data_file_to_mat.py
@@ -14,7 +14,10 @@ def main():
     with open(sys.argv[1]) as f:
         for line in f:
             rf = novelda_pb2.RadarFrame()
-            rf.ParseFromString(base64.b64decode(line))
+            b64_string = line
+            b64_string += '=' * (-len(b64_string) % 4)
+            my_bytes = base64.b64decode(b64_string)
+            rf.ParseFromString(my_bytes)
 
             v = np.array([f for f in rf.range_bins])
             v2 = v[0::2] + v[1::2]*1j
